79584f89e0c6f29df5ac5082464a7a7a858ac386
NelmiEK99 <81624934+NelmiEK99@users.noreply.github.com>
2021-04-04 12:30:25 +0530

    linked navbar
    linked navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent} from './Container/home/home.component'
+import { MedicalComponent } from './medical/medical.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {path:'aboutUs',component: AboutUsComponent},
+  {path:'contactUs',component:ContactUsComponent},
+  {path:'medical',component:MedicalComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { FooterComponent } from './Component/footer/footer.component';
 import { HomeComponent } from './Container/home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MedicalComponent } from './medical/medical.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    MedicalComponent
   ],
   imports: [
     BrowserModule,
