6bf94fbdb527598462628c142d4f597e2715ba0f
NelmiEK99 <81624934+NelmiEK99@users.noreply.github.com>
2021-04-04 13:27:38 +0530

    contact us updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './Container/about-us/about-us.component';
+import { ContactUsComponent } from './Container/contact-us/contact-us.component';
 import { HomeComponent} from './Container/home/home.component'
-import { MedicalComponent } from './medical/medical.component';
+import { MedicalComponent } from './Container/medical/medical.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { FooterComponent } from './Component/footer/footer.component';
 import { HomeComponent } from './Container/home/home.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { MedicalComponent } from './medical/medical.component';
+import { ContactUsComponent } from './Container/contact-us/contact-us.component';
+import { AboutUsComponent } from './Container/about-us/about-us.component';
+import { MedicalComponent } from './Container/medical/medical.component';
 
 @NgModule({
   declarations: [
